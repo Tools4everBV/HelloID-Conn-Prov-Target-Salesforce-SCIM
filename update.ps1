a026d1fbf3b49b001adf41e5ae370aa0d58090c3
Jeroen Blaauw <72070984+JeroenBL@users.noreply.github.com>
2021-09-18 17:01:38 +0200

    Update update.ps1

--- update.ps1
+++ update.ps1
@@ -1,13 +1,13 @@
 #####################################################
-# HelloID-Conn-Prov-Target-SalesForce-Update
+# HelloID-Conn-Prov-Target-Salesforce-Update
 #
-# Version: 1.0.0.0
+# Version: 1.0.0.2
 #####################################################
 $VerbosePreference = 'Continue'
 
 # Initialize default value's
 $config = $configuration | ConvertFrom-Json
-$personObj = $person | ConvertFrom-Json
+$p = $person | ConvertFrom-Json
 $aRef = $AccountReference | ConvertFrom-Json
 $pd = $personDifferences | ConvertFrom-Json
 $success = $false
@@ -66,28 +66,30 @@ function Resolve-HTTPError {
         [object]$ErrorObject
     )
     process {
-        $HttpErrorObj = @{
+        $HttpErrorObj = [PSCustomObject]@{
             FullyQualifiedErrorId = $ErrorObject.FullyQualifiedErrorId
             MyCommand             = $ErrorObject.InvocationInfo.MyCommand
             RequestUri            = $ErrorObject.TargetObject.RequestUri
+            ScriptStackTrace      = $ErrorObject.ScriptStackTrace
+            ErrorMessage          = ''
         }
         if ($ErrorObject.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') {
-            $HttpErrorObj['ErrorMessage'] = $ErrorObject.ErrorDetails.Message
+            $HttpErrorObj.ErrorMessage = $ErrorObject.ErrorDetails.Message
         } elseif ($ErrorObject.Exception.GetType().FullName -eq 'System.Net.WebException') {
             $stream = $ErrorObject.Exception.Response.GetResponseStream()
             $stream.Position = 0
             $streamReader = New-Object System.IO.StreamReader $Stream
             $errorResponse = $StreamReader.ReadToEnd()
-            $HttpErrorObj['ErrorMessage'] = $errorResponse
+            $HttpErrorObj.ErrorMessage = $errorResponse
         }
-        Write-Output "'$($HttpErrorObj.ErrorMessage)', TargetObject: '$($HttpErrorObj.RequestUri), InvocationCommand: '$($HttpErrorObj.MyCommand)"
+        Write-Output $HttpErrorObj
     }
 }
 #endregion
 
 if (-not($dryRun -eq $true)) {
     try {
-        Write-Verbose "Updating account '$($aRef)' for '$($personObj.DisplayName)'"
+        Write-Verbose "Updating account '$($aRef)' for '$($p.DisplayName)'"
         Write-Verbose 'Retrieving accessToken'
         $accessToken = Get-ScimOAuthToken -ClientID $($config.ClientID) -ClientSecret $($config.ClientSecret)
 
@@ -161,34 +163,32 @@ if (-not($dryRun -eq $true)) {
         }
         $results = Invoke-RestMethod @splatParams
         if ($results.id){
-            $logMessage = "Account '$($aRef) for '$($personObj.DisplayName)' successfully updated"
-            Write-Verbose $logMessage
             $success = $true
             $auditLogs.Add([PSCustomObject]@{
-                Message = $logMessage
+                Message = "Update account for: $($p.DisplayName) was successful."
                 IsError = $False
             })
         }
     } catch {
+        $success = $false
         $ex = $PSItem
         if ( $($ex.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.HttpResponseException') -or $($ex.Exception.GetType().FullName -eq 'System.Net.WebException')) {
-            $errorMessage = Resolve-HTTPError -Error $ex
-            $auditMessage = "Account '$($aRef)' for '$($personObj.DisplayName)' not updated. Error: $errorMessage"
+            $errorObj = Resolve-HTTPError -Error $ex
+            $errorMessage = "Could not update salesforce account for: $($p.DisplayName). Error: $($errorObj.ErrorMessage)"
         } else {
-            $auditMessage = "Account '$($aRef)' for '$($personObj.DisplayName)' not updated. Error: $($ex.Exception.Message)"
+            $errorMessage = "Could not update salesforce account for: $($p.DisplayName). Error: $($ex.Exception.Message)"
         }
+        Write-Error $errorMessage
         $auditLogs.Add([PSCustomObject]@{
-                Message = $auditMessage
-                IsError = $true
-            })
-        Write-Error $auditMessage
+            Message = $errorMessage
+            IsError = $true
+        })
+    } finally {
+        $result = [PSCustomObject]@{
+            Success          = $success
+            Account          = $account
+            AuditDetails     = $auditMessage
+        }
+        Write-Output $result | ConvertTo-Json -Depth 10
     }
 }
-
-$result = [PSCustomObject]@{
-    Success          = $success
-    Account          = $account
-    AuditDetails     = $auditMessage
-}
-
-Write-Output $result | ConvertTo-Json -Depth 10
